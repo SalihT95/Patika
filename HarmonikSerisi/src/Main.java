0f841f79a72384b88ace36693b523487b8c0e05c
Salih TÜRKOĞLU <salih>
2023-06-08 17:04:58 +0300

    Girilen sayının Harmonik serisini bulan prog

---
+++ HarmonikSerisi/src/Main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Bir sayı girin: ");
+        int n = scanner.nextInt();
+
+        double harmonikSerisi = 0.0;
+
+        for (int i = 1; i <= n; i++) {
+            harmonikSerisi += 1.0 / i;
+        }
+
+        System.out.println("Harmonik Seri: " + harmonikSerisi);    }
+}
