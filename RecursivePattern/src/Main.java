79a4fa6b18e40e8f2504dde5f89c2b50551be2c2
Salih TÜRKOĞLU <salih>
2023-06-11 02:09:05 +0300

    Recursive düzenlenmiştir

--- RecursivePattern/src/Main.java
+++ RecursivePattern/src/Main.java
@@ -1,29 +1,23 @@
 import java.util.Scanner;
 
 public class Main {
-    static void pattern(){
-        Scanner inp = new Scanner(System.in);
-        System.out.print("Sayı Giriniz : ");
-        int n = inp.nextInt();
-        int firstNum = n;
-        System.out.print("Çıktı :" +n+" ");
-
-        for (int i=n; i>0;i-=5){
-            firstNum-=5;
-            System.out.print(firstNum+" ");
+    static void pattern(int n) {
+        if (n <= 0) {
+            System.out.print(n+" ");
+            return;
         }
 
-        int lastNum =firstNum;
-        for (int i=lastNum;i<n;i+=5){
-            lastNum+=5;
-            System.out.print(lastNum+" ");
-
-        }
-        System.out.println();
-        pattern();
+        System.out.print(n+" ");
+        pattern(n - 5);
+        System.out.print(n+" ");
     }
 
     public static void main(String[] args) {
-        pattern();
+        Scanner inp = new Scanner(System.in);
+        System.out.print("Sayı Giriniz : ");
+        int n = inp.nextInt();
+        System.out.print("Çıktı : ");
+        pattern(n);
+
     }
 }
