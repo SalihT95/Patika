5e62d2f9c3b43f966d36ff685b2c6152b45729a1
Salih TÜRKOĞLU <salih>
2023-06-12 16:37:43 +0300

    recursive de değişiklik yapıldı

--- UstAlanProgram/src/Main.java
+++ UstAlanProgram/src/Main.java
@@ -1,20 +1,23 @@
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner inp = new Scanner(System.in);
-        int taban ,us,sonuc=1;
-        System.out.print("Tabanı girin: ");
-        taban = inp.nextInt();
-
-        System.out.print("Üssü girin: ");
-        us = inp.nextInt();
-
-
-        for (int i = 0; i < us; i++) {
-            sonuc *= taban;
+    static int exponentiation(int base,int exponent){
+        if (exponent==0){
+            return 1;
+        }else {
+            return base*(exponentiation(base,exponent-1));
         }
 
-        System.out.println("Cevap: " + sonuc);
+    }
+    public static void main(String[] args) {
+        Scanner inp = new Scanner(System.in);
+        int base ,exponent ,result=1;
+        System.out.print("Tabanı girin: ");
+        base = inp.nextInt();
+
+        System.out.print("Üssü girin: ");
+        exponent  = inp.nextInt();
+
+        System.out.println("Cevap: " + exponentiation(base,exponent));
     }
 }
