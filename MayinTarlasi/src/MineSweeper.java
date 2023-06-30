11ccdb2b19f8a0f84cf4d6133e08b8ef5da6f61d
Salih TÜRKOĞLU <salih>
2023-06-30 18:28:43 +0300

    mayın tarlası

---
+++ MayinTarlasi/src/MineSweeper.java
@@ -0,0 +1,19 @@
+import java.util.Random;
+import java.util.Scanner;
+public class MineSweeper {
+    //kodu sonra tamamlayacağım
+    Scanner inp = new Scanner(System.in);
+    Random random = new Random();
+    public int row ;
+    public int cow;
+
+    public MineSweeper(int row, int cow) {
+        this.row=row;
+        this.cow=cow;
+    }
+
+
+    public void run(){
+
+    }
+}
